1e62982af8744d264284a2ab150639827421a33c
Morgan Travis <morgandtravis@gmail.com>
2019-06-24 12:50:01 -0400

    Updated yaml, handler, and serverless app config files for dev stage deployment, added website html

--- handler.js
+++ handler.js
@@ -1,16 +1,16 @@
-'use strict';
+const serverless = require("serverless-http");
+const hbs = require("hbs");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-module.exports.helloWorld = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.set("view engine", "hbs");
 
-  callback(null, response);
-};
+app.get("/", function(req, res) {
+  res.status(200).render("index");
+});
+
+module.exports.awesomesauce= serverless(app);
